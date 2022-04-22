e4c5549b7de7b9ece777a8fd04b56b6064adf39e
Sok4h <mrsokaxd@gmail.com>
2022-04-22 15:51:56 -0500

    feed + viewpager + post card

---
+++ app/src/main/java/com/example/people4animals/MyCasesFragment.kt
@@ -0,0 +1,20 @@
+package com.example.people4animals
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MyCasesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_cases, container, false)
+    }
+
+}
